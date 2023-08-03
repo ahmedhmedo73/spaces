4d00cae31fe6e12910e0161052c00cd2de7456ad
ahmedhmedo73 <ahmedhmedo73@gmail.com>
2023-08-03 22:45:52 +0300

    users api service and interface

---
+++ src/app/features/users/interfaces/users.interface.ts
@@ -0,0 +1,12 @@
+export interface UserFormData {
+  name: string;
+  job: string;
+}
+
+export interface User {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
---
+++ src/app/features/users/services/users/users.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/app/core/environments/environment';
+import { UserFormData } from '../../interfaces/users.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  constructor(private httpClient: HttpClient) {}
+
+  GetUsers(page: number) {
+    return this.httpClient.get(environment.endpoint + 'users?page=' + page);
+  }
+  creatUser(data: UserFormData) {
+    return this.httpClient.post(environment.endpoint + 'users', data);
+  }
+  updateUser(id: number, data: UserFormData) {
+    return this.httpClient.post(environment.endpoint + 'users/' + id, data);
+  }
+  deleteUser(id: number) {
+    return this.httpClient.delete(environment.endpoint + 'users/' + id);
+  }
+}
