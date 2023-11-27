ca7c0292963c35e498962d7013cc616bfa231b0b
ahmedhmedo73 <ahmedhmedo73@gmail.com>
2023-11-27 18:17:14 +0200

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,35 +11,6 @@ export class AppComponent {
   title = 'spaces';
 
   ngOnInit(): void {
-    if (ApplePaySession) {
-      if (ApplePaySession.canMakePayments()) {
-        alert('hi, I can do ApplePay.....');
-        alert('sad');
-        alert(ApplePaySession);
-        const session = new ApplePaySession(1, {
-          currencyCode: 'SAR',
-          countryCode: 'SA',
-          requiredShippingContactFields: ['postalAddress'],
-          lineItems: [{ label: 'subTotalDescr', amount: '11' }],
-          total: {
-            label: 'bag',
-            amount: '100',
-          },
-          supportedNetworks: ['masterCard', 'visa'],
-          merchantCapabilities: [
-            'supports3DS',
-            'supportsEMV',
-            'supportsCredit',
-            'supportsDebit',
-          ],
-        });
-        alert(session);
-        session.onpaymentauthorized = (event: any) => {
-          alert(event);
-        };
-      }
-    }
-
     const messaging = getMessaging();
     alert('Notification permission granted.');
     getToken(messaging, { vapidKey: environment.vapidKey })
