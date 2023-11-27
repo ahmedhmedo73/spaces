fe826adc863bb446644828a16eb362dcec953432
ahmedhmedo73 <ahmedhmedo73@gmail.com>
2023-11-27 15:54:47 +0200

    test apple pay

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,1 +1,30 @@
-console.log('hi');
+if (window.ApplePaySession) {
+    const paymentRequest = {
+        currencyCode: "SAR",
+        countryCode: "SA",
+        requiredShippingContactFields: ["postalAddress"],
+        lineItems: [{ label: "subTotalDescr", amount: 11 }],
+        total: {
+            label: "bag",
+            amount: 100,
+        },
+        supportedNetworks: ["masterCard", "visa"],
+        merchantCapabilities: [
+            "supports3DS",
+            "supportsEMV",
+            "supportsCredit",
+            "supportsDebit",
+        ],
+    };
+    if (window.ApplePaySession.canMakePayments) {
+        alert("hi, I can do ApplePay.....");
+        alert("sad");
+        alert(window.ApplePaySession);
+        alert(new window.ApplePaySession(1, paymentRequest));
+        const session = new window.ApplePaySession(1, paymentRequest);
+        alert("session", session);
+        session.onpaymentauthorized = (event) => {
+            alert(event);
+        };
+    }
+}
